eda16a3e4d43d081d54d0ff8f9658b155df7eab2
likithsai <likithsai13@gmail.com>
2022-07-23 16:16:27 +0530

    Minor logic changes for CART page

--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -1,11 +1,18 @@
 //  cart page
 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from './component/Header';
 import Footer from './component/Footer';
 import AppData from '../data/appdata.json';
 
 const CartPage = (props) => {
+    // var itemInCart = JSON.parse(localStorage.getItem('cartData')) || [];
+    const [ itemData, setItemData ] = useState([]);
+
+    useEffect(() => {
+        setItemData(JSON.parse(localStorage.getItem('cartData')));
+    }, []);
+
     return (
         <div className="homepage">
             <Header headerTitle={AppData.appname} logoURL="/" cartURL="/cart" />
@@ -24,7 +31,28 @@ const CartPage = (props) => {
                                                             </tr>
                                                         </thead>
                                                         <tbody>
-                                                            <tr class="border-bottom">
+                                                        {
+                                                            itemData.map((item, index) => {
+                                                                return (
+                                                                    <tr class="border-bottom">
+                                                                        <td class="py-4">
+                                                                            <img src={ item.itemimages[0].imageurl } alt={ item.itemimages[0].imagealt } title="contact-img" class="rounded me-3" height="64" />
+                                                                            <p class="m-0 d-inline-block align-middle font-16">
+                                                                                <a href="apps-ecommerce-products-details.html" class="text-body fw-bold">{ item.itemname }</a>
+                                                                                <br />
+                                                                                <p class="my-0 mb-1">{ item.itemdescshort }</p>
+                                                                                <a href="#">Remove</a>
+                                                                            </p>
+                                                                        </td>
+                                                                        <td class="py-3">
+                                                                            <p class="fw-bold">${ item.itemnewprice }</p>
+                                                                        </td>
+                                                                    </tr>
+                                                                )
+                                                            })
+                                                        }
+
+                                                            {/* <tr class="border-bottom">
                                                                 <td class="py-4">
                                                                     <img src="assets/images/products/product-1.jpg" alt="contact-img" title="contact-img" class="rounded me-3" height="64" />
                                                                     <p class="m-0 d-inline-block align-middle font-16">
@@ -49,7 +77,7 @@ const CartPage = (props) => {
                                                                 <td class="py-3">
                                                                     <p class="fw-bold">$148.66</p>
                                                                 </td>
-                                                            </tr>
+                                                            </tr> */}
                                                         </tbody>
                                                         <tfoot class="table-light">
                                                             <tr class="border-bottom">
@@ -66,7 +94,7 @@ const CartPage = (props) => {
                                             </div>
                                         </div>
                                         <div class="col-md-12 my-5">
-                                            <button type="button" class="btn btn-success p-3 w-100">Checkout</button>  
+                                            <button type="button" class="btn btn-success p-3 w-100">Proceed to  Checkout</button>  
                                         </div>
                                     </div>
                             </div>
