86c920a5f29c8df05cbf8fe5b6e62004ad6b0be8
likithsai <likithsai13@gmail.com>
2022-08-05 21:24:36 +0530

    minor code changes

--- src/pages/ProductPage.js
+++ src/pages/ProductPage.js
@@ -24,27 +24,6 @@ const ProductPage = (props) => {
 
     const addToCart = (item) => {
         dispatch(addItemToCart(item));
-        // var itemInCart = JSON.parse(localStorage.getItem('cartData')) || [];
-        // if(itemInCart !== null) {
-        //     if(!itemInCart.find(x => x.itemid === item.itemid)) {
-        //         itemInCart.push({
-        //             "itemid": item.itemid,
-        //             "itemname": item.itemname,
-        //             "itemdescshort": item.itemdescshort,
-        //             "itemimages": item.itemimages,
-        //             "itemnewprice": item.itemnewprice
-        //         });
-        //     }
-        // } else {
-        //     itemInCart.push({
-        //         "itemid": item.itemid,
-        //         "itemname": item.itemname,
-        //         "itemdescshort": item.itemdescshort,
-        //         "itemimages": item.itemimages,
-        //         "itemnewprice": item.itemnewprice
-        //     });
-        // }
-        // localStorage.setItem('cartData', JSON.stringify(itemInCart));
     }
     
 
