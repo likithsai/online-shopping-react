38661f10b249e36526ae34ab260c25e05442b175
likithsai <likithsai13@gmail.com>
2022-08-06 07:57:23 +0530

    Minor changes in footer

--- src/pages/component/Footer.js
+++ src/pages/component/Footer.js
@@ -4,18 +4,7 @@ import React from 'react';
 const Footer = (props) => {
     return (
         <footer className="py-5 bg-dark">
-            <div className="container">
-                <div class="d-flex flex-wrap justify-content-between align-items-center">
-                    <p class="col-md-4 mb-0 text-white">© 2022 Company, Inc</p>
-                    <ul class="nav col-md-4 justify-content-end">
-                        <li class="nav-item"><a href="#" class="nav-link px-2 text-white">Home</a></li>
-                        <li class="nav-item"><a href="#" class="nav-link px-2 text-white">Features</a></li>
-                        <li class="nav-item"><a href="#" class="nav-link px-2 text-white">Pricing</a></li>
-                        <li class="nav-item"><a href="#" class="nav-link px-2 text-white">FAQs</a></li>
-                        <li class="nav-item"><a href="#" class="nav-link px-2 text-white">About</a></li>
-                    </ul>
-                </div>
-            </div>
+            <div className="container"><p className="m-0 text-center text-white">{props.content}</p></div>
         </footer>
     );
 }
