749f7a2ed4f5fb9ed61d0de570264ef7b4b662a3
likithsai <likithsai13@gmail.com>
2022-08-14 21:01:08 +0530

    minor changes in login component for mobile

--- src/pages/component/Header.js
+++ src/pages/component/Header.js
@@ -21,23 +21,24 @@ const Header = (props) => {
     return (
         <nav className="header">
             <LoginModal show={showLoginModal} onHide={() => setShowLoginModal(false)} onSuccessCallback={() => setShowLoginModal(false)} />
-            <div class="collapse bg-dark" id="navbarHeader">
-                <div class="container">
-                    <div class="row p-0">
-                        <ul class="list-group list-group-flush w-100 p-0">
-                            <li class="list-group-item bg-light d-flex align-items-center justify-content-between px-4">
+            <div className="collapse bg-dark" id="navbarHeader">
+                <div className="container">
+                    <div className="row p-0">
+                        <ul className="list-group list-group-flush w-100 p-0">
+                            <li className="list-group-item bg-light d-flex align-items-center justify-content-between px-4">
                                 <div>
-                                    <i class="bi bi-list me-3"></i>
+                                    <i className="bi bi-list me-3"></i>
                                     <span>Orders</span>
                                 </div>
                                 <span className="badge bg-dark text-white ms-1 px-2 rounded-pill">
                                     <span>{ cartCount }</span>
                                 </span>
                             </li>
-                            <li class="list-group-item bg-light px-4" onClick={() => { 
+                            <li className="list-group-item bg-light px-4" onClick={() => { 
                                 dispatch(logout([]))
+                                window.location.reload();
                             }}>
-                                <i class="bi bi-box-arrow-right me-3"></i>
+                                <i className="bi bi-box-arrow-right me-3"></i>
                                 <span className="text-danger fw-bold">Logout</span>
                             </li>
                         </ul>
@@ -59,32 +60,36 @@ const Header = (props) => {
                         </button>
                         {
                             (loginSession.isLoggedIn) ? (
-                                <Dropdown className="btn d-none d-lg-block m-0">
-                                    <Dropdown.Toggle variant="dark" id="dropdown-basic">{ loginSession.user[0].userName }</Dropdown.Toggle>
-                                    <Dropdown.Menu>
-                                        <Dropdown.Item href="#/action-1">
-                                            <i class="bi bi-list me-3"></i>
-                                            <span>Orders</span>
-                                        </Dropdown.Item>
-                                        <Dropdown.Divider />
-                                        <Dropdown.Item onClick={() =>  { 
-                                            dispatch(logout([]))
-                                        }}>
-                                            <i class="bi bi-box-arrow-in-right me-3"></i>
-                                            <span className="text-danger fw-bold">Logout</span>
-                                        </Dropdown.Item>
-                                    </Dropdown.Menu>
-                                </Dropdown>
+                                <>
+                                    <Dropdown className="btn d-none d-lg-block m-0">
+                                        <Dropdown.Toggle variant="dark" id="dropdown-basic">{ loginSession.user[0].userName }</Dropdown.Toggle>
+                                        <Dropdown.Menu>
+                                            <Dropdown.Item href="#/action-1">
+                                                <i className="bi bi-list me-3"></i>
+                                                <span>Orders</span>
+                                            </Dropdown.Item>
+                                            <Dropdown.Divider />
+                                            <Dropdown.Item onClick={() =>  { 
+                                                dispatch(logout([]))
+                                            }}>
+                                                <i className="bi bi-box-arrow-in-right me-3"></i>
+                                                <span className="text-danger fw-bold">Logout</span>
+                                            </Dropdown.Item>
+                                        </Dropdown.Menu>
+                                    </Dropdown>
+                                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
+                                        <span className="navbar-toggler-icon"></span>
+                                    </button>
+                                </>
                             ) : (
-                                <button className="btn btn-dark d-none d-lg-block" onClick={() => setShowLoginModal(true)}>
-                                    <span>Login</span>
-                                </button>
+                                <>
+                                    <button className="btn btn-dark d-none d-lg-block" onClick={() => setShowLoginModal(true)}>
+                                        <span>Login</span>
+                                    </button>
+                                    <button className="btn d-block d-lg-none btn-dark px-2" onClick={() => setShowLoginModal(true)}>Login</button>
+                                </>
                             )
                         }
-                        
-                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
-                            <span class="bi bi-person-circle"></span>
-                        </button>
                     </div>
                 </div>
             </div>
