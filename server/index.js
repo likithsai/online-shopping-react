9d48ea2544aac2b0f943c93e5688032e4bd2809e
likithsai <likithsai13@gmail.com>
2022-08-24 12:31:51 +0530

    Separated JSON in different files

--- server/index.js
+++ server/index.js
@@ -1,31 +1,11 @@
 //  index.js
 const express = require("express");
 const cors = require("cors");
-const DATABASE = "shoppingcart";
 const mysql = require("mysql");
+const DBQuery = require('./data/DBQuery.json');
+const DATABASE = "shoppingcart";
 const PORT = process.env.PORT || 3001;
 const app = express();
-
-const DBQuery = [
-      `CREATE TABLE tbl_users(
-          user_id int(11) NOT NULL AUTO_INCREMENT,
-          user_name varchar(200) DEFAULT NULL,
-          user_email varchar(200) DEFAULT NULL,
-          user_password varchar(200) NOT NULL,
-          user_createddate timestamp NOT NULL DEFAULT current_timestamp(),
-          PRIMARY KEY (user_id)
-      )`, 
-      `CREATE TABLE tbl_orders(
-          order_id int(11) NOT NULL AUTO_INCREMENT,
-          order_name varchar(200) DEFAULT NULL,
-          order_cusid int(11) DEFAULT NULL,
-          order_createddate timestamp NOT NULL DEFAULT current_timestamp(),
-          PRIMARY KEY (order_id),
-          KEY talk_cusid (order_cusid),
-          CONSTRAINT talk_cusid FOREIGN KEY (order_cusid) 
-          REFERENCES tbl_users(user_id) ON DELETE SET NULL ON UPDATE SET NULL
-      )`
-];
 
 var db = mysql.createConnection({
     host: 'localhost',
@@ -34,17 +14,14 @@ var db = mysql.createConnection({
     database : DATABASE,
 });
 
-const createTables = (db) => {
-    DBQuery.map(itm => {
-        db.query(itm);
-    });
-}
-
 //  connect to database
 db.connect(function(err) {
     if (err) throw err;
-    
-    createTables(db);
+
+    //  create required tables
+    DBQuery.queries.map((itm,ind) => {
+        db.query(itm.query);
+    })
 
     app.use(cors());
 
