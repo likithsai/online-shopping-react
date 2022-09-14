2c374a81000d2e79a4ed3b7d03c6045f57756a92
likithsai <likithsai13@gmail.com>
2022-09-14 10:41:46 +0530

    added few items, minor UI changes, added lazy loading, etc

--- component/Content.js
+++ component/Content.js
@@ -1,10 +1,12 @@
 //  content.js
-
+import Image from 'next/image';
 import React, { useState, useEffect } from 'react';
 
 const Content = (props) => {
     const [ itemData, setItemData ] = useState([]);
     const [ categories, setCategories ] = useState([]);
+    const LIMITAMOUNT = 8;
+    const [ maxLimit, setMaxLimit ] = useState(LIMITAMOUNT);
 
     useEffect(() => {
         setItemData(props.products);
@@ -12,18 +14,27 @@ const Content = (props) => {
         console.log(itemData.length);
     }, [props.products, props.categories]);
 
+    useEffect(() => {
+        window.addEventListener('scroll', function () {
+            if (Math.round(window.innerHeight + window.scrollY) >= document.body.offsetHeight - 300) {
+                setMaxLimit(maxLimit + LIMITAMOUNT);
+                // this.alert('called');
+            }
+        });
+    }, [maxLimit]);
+
     return (
         <main className="pt-5">
             <div className="container px-4 px-lg-5">
                 <div className="row gx-4 gx-lg-5 row-cols-1 row-cols-md-2 row-cols-xl-4 justify-content-center">
                 {
                     (itemData.length > 0) ? (
-                        itemData.map((item, index, length) => {
+                        itemData.slice(0, maxLimit).map((item, index, length) => {
                             return (
                                 <div className="col mb-5 px-2" key={index}>
                                     <div className="card shadow-sm h-100">
                                         <a className="text-decoration-none">
-                                            <img loading="lazy" className="card-img-top" src={item.itemimages[0].imageurl} alt={item.itemimages[0].imagealt} />
+                                            <Image width="100%" height="100%" layout="responsive" objectFit="contain" className="card-img-top" src={item.itemimages[0].imageurl} alt={item.itemimages[0].imagealt} />
                                             <div className="card-body p-4">
                                                 <span className="h5 fw-bolder text-dark text-decoration-none">{item.itemname}</span>
                                                 <p className="item-shorttext mb-3 text-dark">{item.itemdescshort}</p>
@@ -34,6 +45,13 @@ const Content = (props) => {
                                                 </div>
                                             </div>
                                         </a>
+                                        <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                                            <div className="text-center d-flex align-items-center justify-content-between">
+                                                <button className="btn btn-outline-dark mt-auto w-100" onClick={(e) =>alert('clicked')}>
+                                                    <span>Add to cart</span>
+                                                </button>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
                             )
