038a2b95792df08b7ca6eaee20b092fcb8962962
likithsai <likithsai13@gmail.com>
2022-09-14 11:25:50 +0530

    Minor changes

--- component/Content.js
+++ component/Content.js
@@ -22,9 +22,9 @@ const Content = (props) => {
     }, [maxLimit]);
 
     return (
-        <main className="pt-5">
+        <main>
             <div className="container px-4 px-lg-5">
-                <div className={ (itemData.length > 0) ? "row justify-content-center gx-4 gx-lg-5 row-cols-1 row-cols-md-2 row-cols-xl-4" : "row justify-content-center" }>
+                <div className={ (itemData.length > 0) ? "row justify-content-center gx-4 gx-lg-5 row-cols-1 row-cols-md-2 row-cols-xl-4 pt-5" : "row justify-content-center" }>
                 {
                     (itemData.length > 0) ? (
                         itemData.slice(0, maxLimit).map((item, index, length) => {
@@ -55,18 +55,14 @@ const Content = (props) => {
                             )
                         })
                     ) : (
-                        <>
-                            <div className="container">
-                                <div className="row px-lg-5">
-                                    <div className="col-md-12 text-center my-5 h-100">
-                                        <div className="my-3">
-                                            <h1 className="h1 fw-bolder">No Items</h1>
-                                            <div className="error-details">Your cart is empty. Please add items to cart.</div>
-                                        </div>
-                                    </div>
+                        <div className="row px-lg-5">
+                            <div className="col-md-12 text-center my-5 h-100">
+                                <div className="my-3">
+                                    <h1 className="h1 fw-bolder">No Items</h1>
+                                    <div className="error-details">There is no item present here.</div>
                                 </div>
                             </div>
-                        </>
+                        </div>
                     )
                 }
                 </div>
