057e384a6393d9e0a056fd7b3e5e9d76e7461756
likith sai <likithsai13@gmail.com>
2022-10-18 11:29:20 +0530

    Added login condition for order page

--- pages/orders.js
+++ pages/orders.js
@@ -2,21 +2,33 @@ import React, { useState, useEffect } from 'react';
 import Header from '../component/Header';
 import Footer from '../component/Footer';
 import OrderJumboltron from "../component/OrderJumboltron";
+import { useSelector, useDispatch } from 'react-redux';
+import LoginModal from '../component/LoginModal';
 
 const Orders = (props) => {
+
+    const loginSession = useSelector((state) => state.loginSession);
 
     useEffect(() => {
     });
 
-    return (
-        <>
-            <Header headerTitle="Shopping App" logoURL="/" cartURL="/cart" />
-            <main>
-                <OrderJumboltron jumboltronTitle="Total Items Ordered" jumboltronSubtitle={"10"} />
-            </main>
-            <Footer copyrightText="© 2022 Company, Inc" />
-        </>
-    );
+    if(loginSession.isLoggedIn) {
+        return (
+            <>
+                <Header headerTitle="Shopping App" logoURL="/" cartURL="/cart" />
+                <main>
+                     <OrderJumboltron jumboltronTitle="Total Items Ordered" jumboltronSubtitle={"10"} />
+                </main>
+                <Footer copyrightText="© 2022 Company, Inc" />
+            </>
+        );
+    } else {
+        return (
+            <>
+            <p>not logged in</p>
+            </>
+        );
+    }
 }
 
 export default Orders;
