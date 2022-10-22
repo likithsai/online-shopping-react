101272c2ef874153fc933e723e7bd0ec694876e1
likith sai <likithsai13@gmail.com>
2022-10-22 21:28:15 +0530

    UI component changes for order

--- pages/orders.js
+++ pages/orders.js
@@ -4,31 +4,36 @@ import Footer from '../component/Footer';
 import OrderJumboltron from "../component/OrderJumboltron";
 import { useSelector, useDispatch } from 'react-redux';
 import LoginModal from '../component/LoginModal';
+import Link from "next/link";
 
 const Orders = (props) => {
-
-    const loginSession = useSelector((state) => state.loginSession);
-
-    useEffect(() => {
-    });
-
-    if(loginSession.isLoggedIn) {
-        return (
-            <>
-                <Header headerTitle="Shopping App" logoURL="/" cartURL="/cart" />
-                <main>
-                     <OrderJumboltron jumboltronTitle="Total Items Ordered" jumboltronSubtitle={"10"} />
-                </main>
-                <Footer copyrightText="© 2022 Company, Inc" />
-            </>
-        );
-    } else {
-        return (
-            <>
-            <p>not logged in</p>
-            </>
-        );
-    }
+    return (
+        <>
+            <Header headerTitle="Shopping App" logoURL="/" cartURL="/cart" />
+            <main>
+                <OrderJumboltron jumboltronTitle="Total Items Ordered" jumboltronSubtitle={"10"} />
+                <div class="container px-0 px-lg-5">
+                    <table class="table table-sm table-responsive m-0">
+                        <tbody>
+                            <tr>
+                                <td class="p-3">Mark</td>
+                            </tr>
+                            <tr>
+                                <td class="p-3">Mark</td>
+                            </tr>
+                            <tr>
+                                <td class="p-3">Mark</td>
+                            </tr>
+                            <tr>
+                                <td class="p-3">Mark</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </main>
+            <Footer copyrightText="© 2022 Company, Inc" />
+        </>
+    );
 }
 
 export default Orders;
